afbdec5a9a500a62021b5374210e2aebbfd8dd82
TriDivide <liamsmitha@gmail.com>
2020-04-18 12:17:24 +0100

    - added an item to represent a fragment. - got item interaction working for the fragment. for now it outputs to the debug console.

---
+++ Assets/Scripts/Controllers/PlayerItemColisionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerItemColisionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "ItemFragmentPickup") {
+            pickupFragment(other.gameObject);
+        } 
+    }
+
+    private void pickupFragment(GameObject fragment) {
+        Debug.Log("Touched the fragment");
+    }
+
+}
