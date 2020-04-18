e0d6a5c3e5f66c8499a90efcc651ee27a9cecc80
TriDivide <liamsmitha@gmail.com>
2020-04-18 11:48:31 +0100

    - added the variables for the player controller - started work on the movement code for the player controller.

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float mouseSpeed = 3f;
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 tilt = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+    }
+
+    
+}
