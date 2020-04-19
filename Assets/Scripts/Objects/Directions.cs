405957892cddddbf6b58b90ec2b9210baa84d019
TriDivide <liamsmitha@gmail.com>
2020-04-19 20:45:28 +0100

    - created custom objects to help with room generation     - An intVector2 object to handle coordinates     - A Directions Object/Enum to handle spawning directions

---
+++ Assets/Scripts/Objects/Directions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Directions {
+
+    public const int Count = 4;
+
+    public static Direction RandomValue {
+        get {
+            return (Direction)Random.Range(0, Count);
+        }
+    }
+
+    private static IntVector2[] vectors = {
+        new IntVector2(0, 1),
+        new IntVector2(1, 0),
+        new IntVector2(0, -1),
+        new IntVector2(-1, 0)
+    };
+
+    public static IntVector2 ToIntVector2 (this Direction direction) {
+        return vectors[(int)direction];
+    }
+}
+
+public enum Direction {
+    North,
+    East, 
+    South,
+    West
+}
